895dac59d96da795afb3882c6525a7fb60430318
emlin2019 <49819627+R4YVEN@users.noreply.github.com>
2021-08-09 16:14:46 +0200

    added image embedding and big small toggling alda

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 1337;
 
+app.use('/js', express.static('js'));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
-  res.send(__dirname + '/js/message_handler.js');
 });
 
 io.on('connection', (socket) => {
--- js/message_handler.js
+++ js/message_handler.js
@@ -1,18 +1,38 @@
 function ToggleBigImage(imgId)
 {
-    console.log(imgId);
+    var oImg = document.getElementById('img-' + imgId);
+    console.log("Image (" + imgId + ") was clicked!");
+
+    if(oImg.getAttribute('isBig') == "false")
+    {
+        //oImg.removeAttribute('width');
+        //oImg.removeAttribute('heigth');
+        oImg.setAttribute('width', 600);
+        oImg.setAttribute('heigth', 600);
+        oImg.setAttribute('isBig', 'true');
+    }
+    else
+    {
+        oImg.setAttribute('width', 200);
+        oImg.setAttribute('heigth', 200);
+        oImg.setAttribute('isBig', 'false');
+    }
 }
 
-function HandleImage(msgItem)
+function HandleImage(msg, msgItem)
 {
     var imgId = Math.floor(Math.random() * 1000000);
     var oImg = document.createElement("img");
+
+    //Padding
     msgItem.appendChild(document.createElement("br"));
-    oImg.setAttribute('id', imgId);
+    msgItem.appendChild(document.createElement("br"));
+
+    oImg.setAttribute('id', "img-" + imgId);
     oImg.setAttribute('src', msg);
     oImg.setAttribute('width', 200);
     oImg.setAttribute('heigth', 200);
-    oImg.setAttribute('isBig', false);
+    oImg.setAttribute('isBig', "false");
     oImg.setAttribute('onclick', "ToggleBigImage(" + imgId + ");");
     msgItem.appendChild(oImg);
 }
