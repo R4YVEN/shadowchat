94322d8a9e4f601d4ccdafe3dfe700f07a7e32dd
R4YVEN <emlin2019@gmail.com>
2022-10-07 10:09:03 +0200

    initial

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 80;
+const port = 1337;
 
 app.use('/js', express.static('js'));
 app.use('/images', express.static('images'));
@@ -11,27 +11,46 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+var users = [];
 var someoneTyping = false;
 
 io.on('connection', (socket) => {
   var g_username;
+  var g_chatid;
 
-  socket.on('announce_me', (username) => {
-    io.emit('new_user', username);
+  socket.on('announce_me', ({username, chatid}) => {
     g_username = username;
+    g_chatid = chatid;
+    users.push({"id": socket.id, "chatid": chatid});
+
+    for(var user in users)
+    {
+        if(users[user].chatid == g_chatid)
+            io.to(users[user].id).emit('new_user', username, chatid);
+    }
   });
 
   socket.on('typing', (data)=>{
-    if(data.typing==true)
-       io.emit('typing', data)
-    else
-       io.emit('typing', data)
+    for(var user in users)
+    {
+        if(users[user].chatid == g_chatid)
+        {
+            if(data.typing)
+                io.to(users[user].id).emit('typing', data)
+            else
+                io.to(users[user].id).emit('typing', data)
+        }
+    }
   })
 
   socket.on('chat_message', ({ username, msg}) => {
     if(username.length < 12 && username.length > 2)
     {
-      io.emit('chat_message', username, msg);
+        for(var user in users)
+        {
+            if(users[user].chatid == g_chatid)
+                io.to(users[user].id).emit('chat_message', username, msg);
+        }
     }
     else
     {
